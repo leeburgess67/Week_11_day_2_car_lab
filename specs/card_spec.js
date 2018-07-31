974247833e80e7c66c63fc6df036c6184c7b21ae
leeburgess67 <leeburgessmail@gmail.com>
2018-07-31 15:56:51 +0100

    adds card class passing all tests

---
+++ specs/card_spec.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const Card = require('../card.js');
+
+describe('card', function (){
+
+  let card;
+
+  beforeEach(function(){
+    card1 = new Card('superman', 6, 9, 7);
+    card2 = new Card('scarlet witch', 7, 10, 5);
+    card3 = new Card('black widow', 8, 6, 9);
+    card4 = new Card('The Flash',  7, 4, 10);
+    card5 = new Card('Wonder Woman',  8, 7, 5);
+    card6 = new Card('Batman',  10,  5, 6);
+  });
+
+
+  it('should have a name', function(){
+    const result = card1.name;
+    assert.strictEqual(result, 'superman')
+  });
+  it('should have intelligence', function(){
+    const result = card1.intelligence;
+    assert.strictEqual(result, 6)
+  });
+  it('should have strength', function(){
+    const result = card1.strength;
+    assert.strictEqual(result,9)
+  });
+  it('should have agility', function(){
+    const result = card1.agility;
+    assert.strictEqual(result,7)
+  });
+});
